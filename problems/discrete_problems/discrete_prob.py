748785d2e7920e0160ebd3e26ec7ab02af8e9ff2
ngocquan1 <quanphanpq147@gmail.com>
2025-10-29 17:08:57 +0000

    add problem

--- problems/discrete_problems/discrete_prob.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-class DiscreteProblem:
-    def __init__(self):
-        pass
-    def KnapsackProblem(self):
-        pass
-    def TravelingSalesmanProblem(self):
-        pass
